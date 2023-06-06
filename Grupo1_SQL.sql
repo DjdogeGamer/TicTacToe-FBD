43e4c3a1f726a5314fa08f007e5a62d7450890ba
Nathália Couto <79584703+nathaliaacouto@users.noreply.github.com>
2023-06-06 14:54:13 +0100

    Add files via upload

---
+++ Grupo1_SQL.sql
@@ -0,0 +1,29 @@
+CREATE TABLE jogador (
+	idJogador INT NOT NULL UNIQUE,
+	nomeJogador VARCHAR(255),
+    nacionalidade VARCHAR(255),
+    nomeUsuario VARCHAR(255),
+    senha VARCHAR(255) NOT NULL,
+    posicaoRanking INT,
+    vitorias INT,
+    derrotas INT,
+    empates INT,
+    tempoTotalJogo FLOAT,
+    totalJogos INT,
+    PRIMARY KEY (idJogador)
+);
+
+CREATE TABLE jogo (
+	idJogo INT NOT NULL UNIQUE,
+	nome VARCHAR(255),
+    senha VARCHAR(255),
+    modoJogo INT NOT NULL,
+    idJogador1 INT NOT NULL UNIQUE,
+    idJogador2 INT NOT NULL UNIQUE,
+    pontuacao1 INT,
+    pontuacao2 INT,
+	dataJogo DATE,
+    PRIMARY KEY (idJogo),
+    FOREIGN KEY (idJogador1) REFERENCES jogador(idJogador),
+    FOREIGN KEY (idJogador2) REFERENCES jogador(idJogador)
+);
