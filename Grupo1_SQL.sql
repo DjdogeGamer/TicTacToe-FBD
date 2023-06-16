2039b400f11ae5a50f737b0e6ca5b3a666b26c0e
Nathália Couto <79584703+nathaliaacouto@users.noreply.github.com>
2023-06-16 23:09:26 +0100

    Update Grupo1_SQL.sql

--- Grupo1_SQL.sql
+++ Grupo1_SQL.sql
@@ -38,75 +38,6 @@ CREATE TABLE jogo
     FOREIGN KEY (idJogador2) REFERENCES jogador (idJogador)
 );
 
-/* inserção de dados - jogadores */
-
-INSERT INTO jogador(idJogador, nomeJogador, nacionalidade, nomeUsuario, senha, posicaoRanking,
-                    vitorias, derrotas, empates, tempoTotalJogo, totalJogos)
-VALUES (1000, "Calos Eduardo", "Brasil",
-        "carlinhos2023", "adoro_jogar", 10, 100, 20, 30, 5, 150);
-
-INSERT INTO jogador(idJogador, nomeJogador, nacionalidade, nomeUsuario, senha, posicaoRanking,
-                    vitorias, derrotas, empates, tempoTotalJogo, totalJogos)
-VALUES (1467, "Joana Couto", "Portugal",
-        "joaninha", "jo4n4123", 8, 150, 30, 30, 7, 210);
-
-INSERT INTO jogador(idJogador, nomeJogador, nacionalidade, nomeUsuario, senha, posicaoRanking,
-                    vitorias, derrotas, empates, tempoTotalJogo, totalJogos)
-VALUES (3521, "Gonçalo Gomes", "Portugal",
-        "ggomes", "01021997", 25, 40, 3, 7, 1.6, 50);
-
-INSERT INTO jogador(idJogador, nomeJogador, nacionalidade, nomeUsuario, senha, posicaoRanking,
-                    vitorias, derrotas, empates, tempoTotalJogo, totalJogos)
-VALUES (0034, "Kinga Nowak", "Polônia",
-        "queen_123", "polonia123", 12, 100, 50, 10, 5.3, 160);
-
-INSERT INTO jogador(idJogador, nomeJogador, nacionalidade, nomeUsuario, senha, posicaoRanking,
-                    vitorias, derrotas, empates, tempoTotalJogo, totalJogos)
-VALUES (0894, "Maria das Flores", "Brasil",
-        "florida", "melzinha", 1, 250, 20, 30, 10, 300);
-
-INSERT INTO jogador(idJogador, nomeJogador, nacionalidade, nomeUsuario, senha, posicaoRanking,
-                    vitorias, derrotas, empates, tempoTotalJogo, totalJogos)
-VALUES (2043, "Sam Rock", "Canadá",
-        "letsrock", "rock1234", 5, 200, 150, 50, 13.3, 400);
-
-UPDATE jogador
-SET nacionalidade = 'Moçambique'
-WHERE idJogador = 1467;
-
-UPDATE jogador
-SET totalJogos = (SELECT COUNT(idJogo) FROM jogo WHERE idJogador1 = 1000 OR idJogador2 = 1000)
-WHERE idJogador = 1000;
-UPDATE jogador
-SET totalJogos = (SELECT COUNT(idJogo) FROM jogo WHERE idJogador1 = 0034 OR idJogador2 = 0034)
-WHERE idJogador = 0034;
-UPDATE jogador
-SET totalJogos = (SELECT COUNT(idJogo) FROM jogo WHERE idJogador1 = 0894 OR idJogador2 = 0894)
-WHERE idJogador = 0894;
-UPDATE jogador
-SET totalJogos = (SELECT COUNT(idJogo) FROM jogo WHERE idJogador1 = 1467 OR idJogador2 = 1467)
-WHERE idJogador = 1467;
-UPDATE jogador
-SET totalJogos = (SELECT COUNT(idJogo) FROM jogo WHERE idJogador1 = 2043 OR idJogador2 = 2043)
-WHERE idJogador = 2043;
-UPDATE jogador
-SET totalJogos = (SELECT COUNT(idJogo) FROM jogo WHERE idJogador1 = 3521 OR idJogador2 = 3521)
-WHERE idJogador = 3521;
-
-
-/* testes */
-
-SELECT *
-FROM jogador;
-
-SELECT *
-FROM jogo;
-
-/* Limpar tabelas */
-
-DELETE FROM jogo;
-DELETE FROM jogador;
-
 /* inserção de dados - jogos */
 
 INSERT INTO jogo(nome, senha, modoJogo, idJogador1, idJogador2, pontuacao1,
@@ -214,6 +145,124 @@ INSERT INTO jogador (idJogador,
                      totalJogos)
 VALUES (1003, "Bihn Tihn", "Vietname", "tihntihn", "senha123", 1, 15, 5, 1, 65, 55, 29);
 
+UPDATE jogador 
+SET totalJogos = (SELECT COUNT(idJogo) FROM jogo WHERE idJogador1 = 1000 OR idJogador2 = 1000) 
+WHERE idJogador = 1000;
+UPDATE jogador
+SET totalJogos = (SELECT COUNT(idJogo) FROM jogo WHERE idJogador1 = 1001 OR idJogador2 = 1001)
+WHERE idJogador = 1001;
+UPDATE jogador
+SET totalJogos = (SELECT COUNT(idJogo) FROM jogo WHERE idJogador1 = 1002 OR idJogador2 = 1002)
+WHERE idJogador = 1002;
+UPDATE jogador
+SET totalJogos = (SELECT COUNT(idJogo) FROM jogo WHERE idJogador1 = 1003 OR idJogador2 = 1003)
+WHERE idJogador = 1003;
+
+UPDATE jogador SET vitorias = 0 WHERE idJogador = 1000;
+UPDATE jogador SET vitorias = 0 WHERE idJogador = 1001;
+UPDATE jogador SET vitorias = 0 WHERE idJogador = 1002;
+UPDATE jogador SET vitorias = 0 WHERE idJogador = 1003;
+
+UPDATE jogador SET derrotas = 0 WHERE idJogador = 1000;
+UPDATE jogador SET derrotas = 0 WHERE idJogador = 1001;
+UPDATE jogador SET derrotas = 0 WHERE idJogador = 1002;
+UPDATE jogador SET derrotas = 0 WHERE idJogador = 1003;
+
+UPDATE jogador SET empates = 0 WHERE idJogador = 1000;
+UPDATE jogador SET empates = 0 WHERE idJogador = 1001;
+UPDATE jogador SET empates = 0 WHERE idJogador = 1002;
+UPDATE jogador SET empates = 0 WHERE idJogador = 1003;
+
+UPDATE jogador SET pontos = 0 WHERE idJogador = 1000;
+UPDATE jogador SET pontos = 0 WHERE idJogador = 1001;
+UPDATE jogador SET pontos = 0 WHERE idJogador = 1002;
+UPDATE jogador SET pontos = 0 WHERE idJogador = 1003;
+
+UPDATE jogador SET tempoTotalJogo = (totalJogos * 5) WHERE idJogador = 1000;
+UPDATE jogador SET tempoTotalJogo = (totalJogos * 5) WHERE idJogador = 1001;
+UPDATE jogador SET tempoTotalJogo = (totalJogos * 5) WHERE idJogador = 1002;
+UPDATE jogador SET tempoTotalJogo = (totalJogos * 5) WHERE idJogador = 1003;
+
+UPDATE jogador JOIN jogo ON jogador.idJogador = jogo.idJogador2
+SET jogador.vitorias = jogador.vitorias + 1 
+WHERE (jogo.idJogador2 = 1001 AND jogo.pontuacao1 < jogo.pontuacao2);
+UPDATE jogador JOIN jogo ON jogador.idJogador = jogo.idJogador2
+SET jogador.vitorias = jogador.vitorias + 1 
+WHERE (jogo.idJogador2 = 1003 AND jogo.pontuacao1 < jogo.pontuacao2);
+UPDATE jogador JOIN jogo ON jogador.idJogador = jogo.idJogador1
+SET jogador.vitorias = jogador.vitorias + 1 
+WHERE (jogo.idJogador1 = 1000 AND jogo.pontuacao1 > jogo.pontuacao2);
+UPDATE jogador JOIN jogo ON jogador.idJogador = jogo.idJogador1
+SET jogador.vitorias = jogador.vitorias + 1 
+WHERE (jogo.idJogador1 = 1002 AND jogo.pontuacao1 > jogo.pontuacao2);
+
+
+UPDATE jogador SET derrotas = derrotas + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador1
+AND jogo.pontuacao1 < jogo.pontuacao2) WHERE idJogador = 1000;
+UPDATE jogador SET derrotas = derrotas + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador2
+AND jogo.pontuacao1 > jogo.pontuacao2) WHERE idJogador = 1000;
+UPDATE jogador SET derrotas = derrotas + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador1
+AND jogo.pontuacao1 < jogo.pontuacao2) WHERE idJogador = 1001;
+UPDATE jogador SET derrotas = derrotas + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador2
+AND jogo.pontuacao1 > jogo.pontuacao2) WHERE idJogador = 1001;
+UPDATE jogador SET derrotas = derrotas + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador1
+AND jogo.pontuacao1 < jogo.pontuacao2) WHERE idJogador = 1002;
+UPDATE jogador SET derrotas = derrotas + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador2
+AND jogo.pontuacao1 > jogo.pontuacao2) WHERE idJogador = 1002;
+UPDATE jogador SET derrotas = derrotas + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador1
+AND jogo.pontuacao1 < jogo.pontuacao2) WHERE idJogador = 1003;
+UPDATE jogador SET derrotas = derrotas + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador2
+AND jogo.pontuacao1 > jogo.pontuacao2) WHERE idJogador = 1003;
+
+UPDATE jogador SET empates = empates + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador1
+AND jogo.pontuacao1 = jogo.pontuacao2) WHERE idJogador = 1000;
+UPDATE jogador SET empates = empates + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador2
+AND jogo.pontuacao1 = jogo.pontuacao2) WHERE idJogador = 1000;
+UPDATE jogador SET empates = empates + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador1
+AND jogo.pontuacao1 = jogo.pontuacao2) WHERE idJogador = 1001;
+UPDATE jogador SET empates = empates + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador2
+AND jogo.pontuacao1 = jogo.pontuacao2) WHERE idJogador = 1001;
+UPDATE jogador SET empates = empates + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador1
+AND jogo.pontuacao1 = jogo.pontuacao2) WHERE idJogador = 1002;
+UPDATE jogador SET empates = empates + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador2
+AND jogo.pontuacao1 = jogo.pontuacao2) WHERE idJogador = 1002;
+UPDATE jogador SET empates = empates + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador1
+AND jogo.pontuacao1 = jogo.pontuacao2) WHERE idJogador = 1003;
+UPDATE jogador SET empates = empates + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador2
+AND jogo.pontuacao1 = jogo.pontuacao2) WHERE idJogador = 1003;
+
+UPDATE jogador SET pontos = pontos +
+(SELECT SUM(pontuacao1) FROM jogo WHERE jogo.idJogador1 = 1000) 
+WHERE idJogador = 1000;
+UPDATE jogador SET pontos = pontos +
+(SELECT SUM(pontuacao2) FROM jogo WHERE jogo.idJogador2 = 1001) 
+WHERE idJogador = 1001;
+UPDATE jogador SET pontos = pontos +
+(SELECT SUM(pontuacao1) FROM jogo WHERE jogo.idJogador1 = 1002) 
+WHERE idJogador = 1002;
+UPDATE jogador SET pontos = pontos +
+(SELECT SUM(pontuacao2) FROM jogo WHERE jogo.idJogador2 = 1003) 
+WHERE idJogador = 1003;
+
+SELECT * FROM jogador;
+SELECT * FROM jogo;
+
 /* Inserção de dados - Jogos */
 
 INSERT INTO jogo (nome,
@@ -261,6 +310,9 @@ INSERT INTO jogo (nome,
 VALUES ("runaway", "987", 1, 1002, 1003, 30, 20, '2023-06-15', 40);
 
 /* Respondendo aos questionamentos da atividade */
+
+SELECT * FROM jogo;
+SELECT * FROM jogador;
 
 /* 1. Quais os dados pessoais de um dado jogador? */
 SELECT *
