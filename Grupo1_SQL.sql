bb6bea82ef8893ad71d88523a055df87ab1f45c6
Nathália Couto <79584703+nathaliaacouto@users.noreply.github.com>
2023-06-21 23:40:30 +0100

    Update Grupo1_SQL.sql

--- Grupo1_SQL.sql
+++ Grupo1_SQL.sql
@@ -142,6 +142,95 @@ INSERT INTO jogo (nome,
                   tempoJogo)
 VALUES ("runaway", "987", 1, 1002, 1003, 30, 20, '2023-06-15', 40);
 
+INSERT INTO jogo (nome,
+                  senha,
+                  modoJogo,
+                  idJogador1,
+                  idJogador2,
+                  pontuacao1,
+                  pontuacao2,
+                  dataJogo,
+                  tempoJogo)
+VALUES ("cardigan", "395", 1, 1000, 1002, 20, 30, '2023-06-21', 30);
+
+INSERT INTO jogo (nome,
+                  senha,
+                  modoJogo,
+                  idJogador1,
+                  idJogador2,
+                  pontuacao1,
+                  pontuacao2,
+                  dataJogo,
+                  tempoJogo)
+VALUES ("karma", "432", 1, 1000, 1003, 20, 10, '2023-06-21', 20);
+
+INSERT INTO jogo (nome,
+                  senha,
+                  modoJogo,
+                  idJogador1,
+                  idJogador2,
+                  pontuacao1,
+                  pontuacao2,
+                  dataJogo,
+                  tempoJogo)
+VALUES ("mirrorball", "965", 2, 1001, 1002, 30, 30, '2023-06-21', 10);
+
+INSERT INTO jogo (nome,
+                  senha,
+                  modoJogo,
+                  idJogador1,
+                  idJogador2,
+                  pontuacao1,
+                  pontuacao2,
+                  dataJogo,
+                  tempoJogo)
+VALUES ("style", "826", 2, 1001, 1003, 40, 30, '2023-06-21', 20);
+
+INSERT INTO jogo (nome,
+                  senha,
+                  modoJogo,
+                  idJogador1,
+                  idJogador2,
+                  pontuacao1,
+                  pontuacao2,
+                  dataJogo,
+                  tempoJogo)
+VALUES ("enchanted", "231", 1, 1002, 1003, 10, 30, '2023-06-21', 30);
+
+INSERT INTO jogo (nome,
+                  senha,
+                  modoJogo,
+                  idJogador1,
+                  idJogador2,
+                  pontuacao1,
+                  pontuacao2,
+                  dataJogo,
+                  tempoJogo)
+VALUES ("afterglow", "515", 1, 1001, 1000, 20, 20, '2023-06-21', 40);
+
+INSERT INTO jogo (nome,
+                  senha,
+                  modoJogo,
+                  idJogador1,
+                  idJogador2,
+                  pontuacao1,
+                  pontuacao2,
+                  dataJogo,
+                  tempoJogo)
+VALUES ("karma", "441", 2, 1003, 1000, 40, 20, '2023-06-21', 30);
+
+INSERT INTO jogo (nome,
+                  senha,
+                  modoJogo,
+                  idJogador1,
+                  idJogador2,
+                  pontuacao1,
+                  pontuacao2,
+                  dataJogo,
+                  tempoJogo)
+VALUES ("seven", "697", 1, 1001, 1002, 20, 10, '2023-06-21', 20);
+
+
 UPDATE jogador 
 SET totalJogos = (SELECT COUNT(idJogo) FROM jogo WHERE idJogador1 = 1000 OR idJogador2 = 1000) 
 WHERE idJogador = 1000;
@@ -152,25 +241,46 @@ UPDATE jogador
 SET totalJogos = (SELECT COUNT(idJogo) FROM jogo WHERE idJogador1 = 1002 OR idJogador2 = 1002)
 WHERE idJogador = 1002;
 UPDATE jogador 
-SET totalJogos = (SELECT COUNT(idJogo) FROM jogo WHERE idJogador1 = 1003 OR idJogador2 = 1003);
+SET totalJogos = (SELECT COUNT(idJogo) FROM jogo WHERE idJogador1 = 1003 OR idJogador2 = 1003)
+WHERE idJogador = 1003;
 
-UPDATE jogador SET tempoTotalJogo = (totalJogos * 5) WHERE idJogador = 1000;
-UPDATE jogador SET tempoTotalJogo = (totalJogos * 5) WHERE idJogador = 1001;
-UPDATE jogador SET tempoTotalJogo = (totalJogos * 5) WHERE idJogador = 1002;
-UPDATE jogador SET tempoTotalJogo = (totalJogos * 5) WHERE idJogador = 1003;
+UPDATE jogador 
+SET tempoTotalJogo = (SELECT SUM(tempoJogo) FROM jogo WHERE idJogador1 = 1000 OR idJogador2 = 1000) 
+WHERE idJogador = 1000;
+UPDATE jogador 
+SET tempoTotalJogo = (SELECT SUM(tempoJogo) FROM jogo WHERE idJogador1 = 1001 OR idJogador2 = 1001) 
+WHERE idJogador = 1001;
+UPDATE jogador 
+SET tempoTotalJogo = (SELECT SUM(tempoJogo) FROM jogo WHERE idJogador1 = 1002 OR idJogador2 = 1002) 
+WHERE idJogador = 1002;
+UPDATE jogador 
+SET tempoTotalJogo = (SELECT SUM(tempoJogo) FROM jogo WHERE idJogador1 = 1003 OR idJogador2 = 1003) 
+WHERE idJogador = 1003;
 
-UPDATE jogador JOIN jogo ON jogador.idJogador = jogo.idJogador2
-SET jogador.vitorias = jogador.vitorias + 1 
-WHERE (jogo.idJogador2 = 1001 AND jogo.pontuacao1 < jogo.pontuacao2);
-UPDATE jogador JOIN jogo ON jogador.idJogador = jogo.idJogador2
-SET jogador.vitorias = jogador.vitorias + 1 
-WHERE (jogo.idJogador2 = 1003 AND jogo.pontuacao1 < jogo.pontuacao2);
-UPDATE jogador JOIN jogo ON jogador.idJogador = jogo.idJogador1
-SET jogador.vitorias = jogador.vitorias + 1 
-WHERE (jogo.idJogador1 = 1000 AND jogo.pontuacao1 > jogo.pontuacao2);
-UPDATE jogador JOIN jogo ON jogador.idJogador = jogo.idJogador1
-SET jogador.vitorias = jogador.vitorias + 1 
-WHERE (jogo.idJogador1 = 1002 AND jogo.pontuacao1 > jogo.pontuacao2);
+UPDATE jogador SET vitorias = vitorias + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador1
+AND jogo.pontuacao1 > jogo.pontuacao2) WHERE idJogador = 1000;
+UPDATE jogador SET vitorias = vitorias + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador2
+AND jogo.pontuacao1 < jogo.pontuacao2) WHERE idJogador = 1000;
+UPDATE jogador SET vitorias = vitorias + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador1
+AND jogo.pontuacao1 > jogo.pontuacao2) WHERE idJogador = 1001;
+UPDATE jogador SET vitorias = vitorias + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador2
+AND jogo.pontuacao1 < jogo.pontuacao2) WHERE idJogador = 1001;
+UPDATE jogador SET vitorias = vitorias + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador1
+AND jogo.pontuacao1 > jogo.pontuacao2) WHERE idJogador = 1002;
+UPDATE jogador SET vitorias = vitorias + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador2
+AND jogo.pontuacao1 < jogo.pontuacao2) WHERE idJogador = 1002;
+UPDATE jogador SET vitorias = vitorias + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador1
+AND jogo.pontuacao1 > jogo.pontuacao2) WHERE idJogador = 1003;
+UPDATE jogador SET vitorias = vitorias + 
+(SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador2
+AND jogo.pontuacao1 < jogo.pontuacao2) WHERE idJogador = 1003;
 
 UPDATE jogador SET derrotas = derrotas + 
 (SELECT COUNT(*) FROM jogo WHERE jogador.idJogador = jogo.idJogador1
@@ -251,7 +361,7 @@ WHERE nomeJogador = "Ella Heidi";
 SELECT MAX(dataJogo) AS ultimaDataJogo
 FROM jogo
 WHERE idJogador1 = (SELECT idJogador FROM jogador WHERE nomeJogador = "Simona Urbutis")
-   OR idJogador2 = (SELECT idJogador FROM jogador WHERE nomeJogador = "Masha Tarkovsky");
+   OR idJogador2 = (SELECT idJogador FROM jogador WHERE nomeJogador = "Simona Urbutis");
 
 /* 3. Quantos jogos realizaram todos os jogadores? */
 
@@ -259,30 +369,6 @@ SELECT COUNT(idJogo) AS totalJogos
 FROM jogo;
 
 /* 4. Quais os nomes dos jogadores associados a uma dada sala? */
-SELECT j.nomeJogador
-FROM jogador j
-JOIN jogo jo ON j.idJogador = jo.idJogador1 OR j.idJogador = jo.idJogador2
-WHERE jo.nome = 'starman';
-
-SELECT j.nomeJogador
-FROM jogador j
-JOIN jogo jo ON j.idJogador = jo.idJogador1 OR j.idJogador = jo.idJogador2
-WHERE jo.nome = 'hotelcalifornia';
-
-SELECT j.nomeJogador
-FROM jogador j
-JOIN jogo jo ON j.idJogador = jo.idJogador1 OR j.idJogador = jo.idJogador2
-WHERE jo.nome = 'psychokiller';
-
-SELECT j.nomeJogador
-FROM jogador j
-JOIN jogo jo ON j.idJogador = jo.idJogador1 OR j.idJogador = jo.idJogador2
-WHERE jo.nome = 'runaway';
-
-
-/*
-Procura o nome do jogador (1 ou 2) que esta associado (inner join) a um jogo com o nome da sala
-*/
 
 SELECT jogador.nomeJogador
 FROM jogador
@@ -303,16 +389,16 @@ GROUP BY jogador.nomeJogador;
 
 /* 5. Qual o desempenho médio dos jogadores por cada país? */
 
-/* faz a média de vitorias, derrotas e empates agrupando por nacionalidade */
+/* Faz a média de vitorias, derrotas e empates agrupando por nacionalidade */
 
 SELECT nacionalidade,
        AVG(vitorias) AS media_vitorias,
        AVG(derrotas) AS media_derrotas,
        AVG(empates)  AS media_empates
 FROM jogador
-GROUP BY nacionalidade;
+GROUP BY nacionalidade ORDER BY media_vitorias DESC;
 
-/* 6. Quais os três países com melhor desempenho? - tem que ser feito com pontos */
+/* 6. Quais os três países com melhor desempenho? */
 
 SELECT nacionalidade, SUM(pontos) AS total_pontos
 FROM jogador
@@ -320,36 +406,33 @@ GROUP BY nacionalidade
 ORDER BY total_pontos DESC
 LIMIT 3;
 
-/* 7. Qual a percentagem de empates considerando todos os jogos? - alterar para a tabela de jogos */
+/* 7. Qual a percentagem de empates considerando todos os jogos? */
 
 SELECT (COUNT(*) * 100) / (SELECT COUNT(*) FROM jogo) AS percentagem_empates
 FROM jogo
 WHERE pontuacao1 = pontuacao2;
 
-/* empate */
-
-/* Qual o total de tempo despendido por um dado jogador no jogo? */
-
-/* Substituir <id_jogador> pelo id do jogador que quer procurar */
+/* 8. Qual o total de tempo despendido por um dado jogador no jogo? */
 
 SELECT SUM(tempoJogo) AS total_tempo_despendido
 FROM jogo
 WHERE idJogador1 = 1000
-   OR idJogador2 = 1001;
+   OR idJogador2 = 1000;
+   
+SELECT nomeJogador, tempoTotalJogo
+FROM jogador
+WHERE idJogador = 1000;
 
-/* 9. Qual a percentagem de vitórias considerando todos os jogos? - alterar para a tabela de jogos */
+/* 9. Qual a percentagem de vitórias considerando todos os jogos? */
 
 SELECT (SUM(vitorias) * 100) / SUM(totalJogos) AS percentagem_vitorias
 FROM jogador;
 
 /* 10. Quantas vezes um dado jogador jogou no modo online? */
 
-/* substituir <id_jogador> pelo id do jogador que quer procurar */
-/* PS: ver indice de modoJogo */
-
 SELECT COUNT(*) AS total_jogos_online
 FROM jogo
-WHERE (idJogador1 = 1000 OR idJogador2 = 1001)
+WHERE (idJogador1 = 1000 OR idJogador2 = 1000)
   AND modoJogo = 1;
 
 /* EXTRA. Qual a percentagem de vitórias para cada jogador? */
@@ -357,7 +440,7 @@ SELECT nomeJogador, (vitorias * 100) / totalJogos AS percentual_vitorias
 FROM jogador
 ORDER BY percentual_vitorias DESC;
 
-/* Ranking de jogadores */
-SELECT *
+/* EXTRA. Ranking de jogadores */
+SELECT nomeJogador, pontos
 FROM jogador
-ORDER BY posicaoRanking;
+ORDER BY pontos DESC;
